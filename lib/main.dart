cefe0269840cef3ec1ea3638d5e0a05e9c271b7a
dleurs <dimitri.leurs@gmail.com>
2020-10-14 11:11:44 +0200

    both choices of using variable bloc or bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  bool useBlocProvierInsteadOfVarBloc = true;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,16 +19,19 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: BlocProvider<CounterBloc>(
-        create: (context) => CounterBloc(),
-        child: HomePage(title: 'Flutter Demo Home Page'),
-      ),
+      home: (useBlocProvierInsteadOfVarBloc)
+          ? BlocProvider<CounterBloc>(
+              create: (context) => CounterBloc(),
+              child: HomePageBlocProvider(title: 'Flutter Demo Home Page'),
+            )
+          : HomePageVarBloc(title: 'Flutter Demo Home Page'),
+/*        */
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  HomePage({Key key, this.title}) : super(key: key);
+class HomePageBlocProvider extends StatelessWidget {
+  HomePageBlocProvider({Key key, this.title}) : super(key: key);
 
   final String title;
 
@@ -76,6 +80,87 @@ class HomePage extends StatelessWidget {
               BlocProvider.of<CounterBloc>(context)
                   .add(DecrementCounterEvent());
             },
+            tooltip: 'Decrement',
+            child: Icon(Icons.remove),
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class HomePageVarBloc extends StatefulWidget {
+  HomePageVarBloc({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageVarBlocState createState() => _HomePageVarBlocState();
+}
+
+class _HomePageVarBlocState extends State<HomePageVarBloc> {
+  CounterBloc _bloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _bloc = CounterBloc();
+  }
+
+  @override
+  void dispose() {
+    _bloc.close();
+    super.dispose();
+  }
+
+  void _incrementCounter() {
+    _bloc.add(IncrementCounterEvent());
+  }
+
+  void _decrementCounter() {
+    _bloc.add(DecrementCounterEvent());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            BlocBuilder<CounterBloc, CounterState>(
+              cubit: _bloc,
+              builder: (context, state) {
+                return Text(
+                  state.count.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            heroTag: 'IncrementButton',
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          FloatingActionButton(
+            heroTag: 'DecrementButton',
+            onPressed: _decrementCounter,
             tooltip: 'Decrement',
             child: Icon(Icons.remove),
           ),
